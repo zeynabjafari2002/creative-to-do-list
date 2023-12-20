df96448bc3117ce7dd9cfd00e43cf89177c4652e
Zeynab <zeynabjafari2002@gmail.com>
2023-12-20 20:31:41 +0330

    adding to DOM successfully

--- src/index/index.js
+++ src/index/index.js
@@ -24,6 +24,7 @@ function addNewTaskByPencilBtn(){
     }
     inputValue.value=''
     taskArray.push(newTaskObj)
+    todoGenerator(taskArray)
 }
 
 $.addEventListener('keypress' , addNewTaskByEnter)
@@ -38,12 +39,20 @@ function addNewTaskByEnter(event){
         }
         inputValue.value=''
         taskArray.push(newTaskObj)
-       
+        todoGenerator(taskArray)
     }
 }
 
 
+// adding to DOM  
+let tableBody=$.getElementById('tableBody')
 
+function todoGenerator(todoList){
+    tableBody.innerHTML=''
+    todoList.forEach(function tasks(task){
+        tableBody.insertAdjacentHTML("beforeend" , '<tr class="text-white "><td class=" text-center">'+ task.id +'</td><td class=" pl-2">'+task.task+'</td><td class=" text-center">'+task.date+'</td><td class=" text-center cursor-pointer"><i class="uil uil-square-full"></i></td><td class=" text-center cursor-pointer  text-xl"><i class="uil uil-trash-alt  hover:text-red-600"></i></td></tr>' )
+    })
+}
 
 
 
